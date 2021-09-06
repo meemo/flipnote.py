31ed601653d0dd668c4c8d4bf8af06d14267ff57
meemo <meemo4556@gmail.com>
2021-09-06 01:41:14 -0400

    More hierarchy changes, more to setup.py, for 0.0.3

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A Python library for Flipnote Studio (3D) files.
 
 This is based on [kwz-parser](https://github.com/Flipnote-Collective/kwz-parser) and 
 [ppm-parser](https://github.com/Flipnote-Collective/ppm-parser) by the flipnote collective.
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name="flipnote",
+      version="0.0.3",
+      description="A Python library for Flipnote Studio (3D) files",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Meemo",
+      author_email="meemo4556@gmail.com",
+      license="MIT",
+      install_requires=[
+          "numpy"
+      ],
+      url="https://github.com/pypa/sampleproject",
+      project_urls={
+          "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      package_dir={"": "src"},
+      packages=find_packages(where="src"),
+      python_requires=">=3.7")
